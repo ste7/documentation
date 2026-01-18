37e24f1256297269c82a8fcf1f4c007bbd9d2584
Stefan Babic <stefanbabicbg@gmail.com>
2026-01-18 16:15:26 +0100

    b95830

---
+++ ahrefs-analytics.js
@@ -0,0 +1,8 @@
+// Load Ahrefs Analytics script with data-key attribute
+(function() {
+  var script = document.createElement('script');
+  script.src = 'https://analytics.ahrefs.com/analytics.js';
+  script.setAttribute('data-key', 'nm8GuGFTT4iOwml7EPYMzQ');
+  script.async = true;
+  document.head.appendChild(script);
+})();
